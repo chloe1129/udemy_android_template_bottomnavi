8bd833d62f7d227eb7b712ad833decee7a3a84fa
Chaehy <chlee7670@cau.ac.kr>
2022-03-25 05:43:10 +0900

    add: week2

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -5,25 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.flo.databinding.FragmentHomeBinding
+import com.example.flo.databinding.FragmentAlbumBinding
 
-class HomeFragment : Fragment() {
+class AlbumFragment : Fragment() {
 
-    lateinit var binding: FragmentHomeBinding
+    lateinit var binding: FragmentAlbumBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater,container,false)
 
-        binding.homeAlbumImgIv1.setOnClickListener {
+        binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.main_frm , AlbumFragment())
+                .replace(R.id.main_frm, HomeFragment())
                 .commitAllowingStateLoss()
         }
 
         return binding.root
     }
+
 }
--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.flo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.flo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initBottomNavigation()
+
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+        }
+
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this,SongActivity::class.java)
+            intent.putExtra("title", song.title)
+            intent.putExtra("singer",song.singer)
+            startActivity(intent)
+        }
 
     }
 
