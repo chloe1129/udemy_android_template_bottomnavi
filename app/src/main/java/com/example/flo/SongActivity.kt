653fc53797ace2bd7ec851dca84b010bf1959920
Chaehy <chlee7670@cau.ac.kr>
2022-04-06 19:53:15 +0900

    add: week4

--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySongBinding
@@ -8,15 +9,17 @@ import com.example.flo.databinding.ActivitySongBinding
 class SongActivity : AppCompatActivity(){
 
     lateinit var binding: ActivitySongBinding
+    lateinit var song: Song
+    lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if(intent.hasExtra("title") && intent.hasExtra("singer")){
-            binding.songNameTv.text = intent.getStringExtra("title")
-            binding.songSingerTv.text = intent.getStringExtra("singer")
-        }
+
+        initSong()
+        setPlayer(song)
+
 
         binding.songDownIb.setOnClickListener{
             finish()
@@ -29,7 +32,39 @@ class SongActivity : AppCompatActivity(){
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.interrupt()
+    }
+
+    private fun initSong(){
+        if (intent.hasExtra("title") && intent.hasExtra("singer")){
+            song=Song(
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second",0)!!,
+                intent.getIntExtra("playTime",0)!!,
+                intent.getBooleanExtra("isPlaying",false)!!,
+
+            )
+        }
+        startTimer()
+    }
+
+    private fun setPlayer(song: Song){
+        binding.songNameTv.text = intent.getStringExtra("title")
+        binding.songSingerTv.text = intent.getStringExtra("singer")
+        binding.songStartTimeTv.text= String.format("%02d:%02d", song.second/60, song.second%60)
+        binding.songEndTimeTv.text= String.format("%02d:%02d", song.playTime/60, song.playTime%60)
+        binding.songProgressSb.progress=(song.second*10000/song.playTime)
+
+        setPlayerStatus(song.isPlaying)
+    }
+
     fun setPlayerStatus(isPlaying : Boolean){
+        song.isPlaying=isPlaying
+        timer.isPlaying=isPlaying
+
         if(isPlaying){
             binding.songMiniplayerIv.visibility= View.VISIBLE
             binding.songPauseIv.visibility=View.GONE
@@ -37,6 +72,46 @@ class SongActivity : AppCompatActivity(){
         else{
             binding.songMiniplayerIv.visibility= View.GONE
             binding.songPauseIv.visibility=View.VISIBLE
+        }
+    }
+
+    private fun startTimer(){
+        timer = Timer(song.playTime,song.isPlaying)
+        timer.start()
+    }
+
+    inner class Timer(private val playTime: Int, var isPlaying: Boolean= true):Thread(){
+
+        private var second : Int=0
+        private var mills: Float=0f
+
+        override fun run() {
+            super.run()
+            try {
+                while (true){
+                    if (second >= playTime){
+                        break
+                    }
+                    if (isPlaying){
+                        sleep(50)
+                        mills +=50
+
+                        runOnUiThread {
+                            binding.songProgressSb.progress = ((mills / playTime)*100).toInt()
+                        }
+                        if (mills % 1000 == 0f){
+                            runOnUiThread {
+                                binding.songStartTimeTv.text= String.format("%02d:%02d", second/60, second%60)
+
+                            }
+                            second++
+                        }
+                    }
+                }
+            }catch (e: InterruptedException){
+                Log.d("Song", "쓰레드가 죽었습니다. ${e.message}")
+            }
+
         }
     }
 }
